7c73fe553177b2955d41eb0819815b51ab151322
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-02-22 10:13:05 +0000

    Create 8.2.c

---
+++ 8.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+	char a[100];
+	int i,flag=0;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	 if(a[i]=='a' || a[i]=='e' ||a[i]=='i' ||a[i]=='o' || a[i]=='u')
+	  flag++;
+	  if(flag>0)
+	  {
+	  printf("yes");
+	  }
+	  else
+	  {
+	  printf("no");
+	  }
+}
