9f5aeb29b341936644ae3da5d3f3d62357ccc740
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-03-20 15:52:21 +0530

    Create 8.11.c

---
+++ 8.11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+printf("Enter the sentence:");
+scanf("%[^\n]%*c",a);
+int k,i,c=1;
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+c++;
+}
+printf("The total number of words is %d",c);
+return 0;
+}
